27c704b6c12e2ec19fdd95c3d3148e02b28efdeb
torikaraage1234 <makiraakira@yahoo.co.jp>
2025-06-07 18:31:57 +0900

    upload

---
+++ test/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.slider').slick({
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      fade: true,
+      // centerMode: true,
+      // centerPadding: "30px",
+      // slidesToShow: 3,
+      // speed: 400,
+   });
+});
